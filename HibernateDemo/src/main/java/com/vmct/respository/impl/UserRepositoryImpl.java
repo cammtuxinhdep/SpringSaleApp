03bffb7e0578ea61d6d29da20ed463a3d846a2ae
cammtuxinhdep <camstuscools@gmail.com>
2025-07-03 14:53:58 +0700

    cart & receipt

---
+++ HibernateDemo/src/main/java/com/vmct/respository/impl/UserRepositoryImpl.java
@@ -0,0 +1,18 @@
+///*
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+// */
+//package com.vmct.respository.impl;
+//
+//import com.vmct.hibernatedemo.HibernateUtils;
+//import com.vmct.pojo.User;
+//
+///**
+// *
+// * @author HP
+// */
+//public class UserRepositoryImpl {
+//    public User getUserByUsername(String username) {
+//        //Session s = HibernateUtils.getFACTORY().op
+//    }
+//}
